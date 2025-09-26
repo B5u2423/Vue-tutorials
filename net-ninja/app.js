594288eaba2493a85d3e3fc98294c64451e5ce32
B5u2423 <masterofyordle@gmail.com>
2025-09-26 17:51:32 +0700

    lesson 2

--- net-ninja/app.js
+++ net-ninja/app.js
@@ -8,7 +8,17 @@ const app = Vue.createApp({
         return {
             title: "Random Title",
             author: "Author Name",
-            age: 20
+            age: 1,
+            x: 0,
+            y: 0,
+            books: [
+                { title: "Book 1", author: "Author 1", img: "assets/1.png" },
+                { title: "Book 2", author: "Author 2", img: "assets/2.png" },
+                { title: "Book 3", author: "Author 3", img: "assets/3.png" },
+                { title: "Book 4", author: "Author 4", img: "assets/4.png" },
+                { title: "Book 5", author: "Author 5", img: "assets/5.png" }
+            ],
+            url: "https://example.com"
         }
         // data can not be used outside of Vue app
     },
@@ -17,6 +27,17 @@ const app = Vue.createApp({
     methods: { // properties that are functions
         decreaseAge() {
             this.age--
+        },
+        handleEvent(e, data) {
+            console.log(e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+            console.log(this.x, this.y)
         }
     }
 })
