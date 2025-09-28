03bf653c4e09061e9d27b199d73f079645bffc8d
B5u2423 <masterofyordle@gmail.com>
2025-09-28 19:22:04 +0700

    lesson 8: router
    simple job listing demo to work with router and urls

--- net-ninja/simple-jobs/src/App.vue
+++ net-ninja/simple-jobs/src/App.vue
@@ -1,10 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link :to="{name: 'about'}">About</router-link> |
+      <router-link :to="{name: 'jobs'}">Jobs</router-link>
+    </nav>
+
+    <button @click="redirect">Redirect</button>
+    <button @click="goBack">Go Back</button>
+    <button @click="goForward">Go Forward</button>
+
+    <router-view/>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    // navigate programmatically in Vue3
+    redirect() {
+      this.$router.push({name: 'home'})
+    },
+    goBack() {
+      this.$router.go(-1)
+    },
+    goForward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -22,9 +47,21 @@ nav {
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
+  padding: 10px;
+  border-radius: 10px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 }
 </style>
--- net-ninja/simple-jobs/src/views/HomeView.vue
+++ net-ninja/simple-jobs/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora itaque sequi aliquid eligendi iste at cupiditate dolores quasi esse ipsa minus officiis, blanditiis nostrum hic excepturi provident? Animi ipsum quaerat harum officia obcaecati laudantium, quisquam similique labore eos vel unde ipsa deleniti cum placeat facere debitis iure et id accusantium magnam mollitia velit sequi autem. Delectus, exercitationem quisquam earum aut magnam error, reiciendis quam nisi esse sed dolorum mollitia sapiente suscipit perferendis! Quia maiores possimus excepturi explicabo animi accusantium qui ullam reiciendis voluptate suscipit placeat vero repellendus dicta fuga, repudiandae porro ipsa corrupti. Aperiam deserunt sunt, recusandae tenetur impedit maxime!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora itaque sequi aliquid eligendi iste at cupiditate dolores quasi esse ipsa minus officiis, blanditiis nostrum hic excepturi provident? Animi ipsum quaerat harum officia obcaecati laudantium, quisquam similique labore eos vel unde ipsa deleniti cum placeat facere debitis iure et id accusantium magnam mollitia velit sequi autem. Delectus, exercitationem quisquam earum aut magnam error, reiciendis quam nisi esse sed dolorum mollitia sapiente suscipit perferendis! Quia maiores possimus excepturi explicabo animi accusantium qui ullam reiciendis voluptate suscipit placeat vero repellendus dicta fuga, repudiandae porro ipsa corrupti. Aperiam deserunt sunt, recusandae tenetur impedit maxime!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora itaque sequi aliquid eligendi iste at cupiditate dolores quasi esse ipsa minus officiis, blanditiis nostrum hic excepturi provident? Animi ipsum quaerat harum officia obcaecati laudantium, quisquam similique labore eos vel unde ipsa deleniti cum placeat facere debitis iure et id accusantium magnam mollitia velit sequi autem. Delectus, exercitationem quisquam earum aut magnam error, reiciendis quam nisi esse sed dolorum mollitia sapiente suscipit perferendis! Quia maiores possimus excepturi explicabo animi accusantium qui ullam reiciendis voluptate suscipit placeat vero repellendus dicta fuga, repudiandae porro ipsa corrupti. Aperiam deserunt sunt, recusandae tenetur impedit maxime!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora itaque sequi aliquid eligendi iste at cupiditate dolores quasi esse ipsa minus officiis, blanditiis nostrum hic excepturi provident? Animi ipsum quaerat harum officia obcaecati laudantium, quisquam similique labore eos vel unde ipsa deleniti cum placeat facere debitis iure et id accusantium magnam mollitia velit sequi autem. Delectus, exercitationem quisquam earum aut magnam error, reiciendis quam nisi esse sed dolorum mollitia sapiente suscipit perferendis! Quia maiores possimus excepturi explicabo animi accusantium qui ullam reiciendis voluptate suscipit placeat vero repellendus dicta fuga, repudiandae porro ipsa corrupti. Aperiam deserunt sunt, recusandae tenetur impedit maxime!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: { }
 }
 </script>
