39a6a9c43aa2f762ff11a8d82e59f4301735118d
B5u2423 <masterofyordle@gmail.com>
2025-09-29 17:54:46 +0700

    lesson 10: bootstrap project

---
+++ net-ninja/dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <h2>{{ name }} - {{ age }}</h2>
+    <button @click="handleClick">Click me</button>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HomeView',
+  components: { },
+  // lifecycle hook composition api
+  setup() {
+    console.log("setup")
+
+    // const make the reference constant, not the value
+    // variables with ref() will be reactive
+    const name = ref("mario")
+    const age = ref(30)
+
+    // variables are not reactive
+    // do it with `refs`
+    const handleClick = () => {
+      name.value = "luigi"
+      age.value = 45
+    }
+
+    return {name, age, handleClick}
+  }
+}
+</script>
