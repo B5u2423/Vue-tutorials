710a34caf5ed712cd04de819a492f0896153bb87
B5u2423 <masterofyordle@gmail.com>
2025-09-29 21:48:00 +0700

    lessong 10: working with composition api prt 1

--- net-ninja/dojo-blog/src/views/HomeView.vue
+++ net-ninja/dojo-blog/src/views/HomeView.vue
@@ -4,11 +4,15 @@
     <h2>{{ name }} - {{ age }}</h2>
     <button @click="handleClick">Click me</button>
     <input type="text" v-model="name">
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <!-- <div v-for="tmp in names" :key="tmp"> {{ tmp }} </div> -->
+    <div v-for="matchingName in matchingNames" :key="matchingName"> {{ matchingName }} </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -29,7 +33,24 @@ export default {
       age.value = 45
     }
 
-    return {name, age, handleClick}
+    // computed: search and filter
+    const search = ref('')
+    const names = ref(['peach', 'mario', 'yoshi', 'bowser', 'luigi', 'jake', 'finn', 'wario', 'waluigi'])
+
+    // watch and watchEffect: inspect the component
+    watch(search, () => {
+      console.log('watch function ran')
+    })
+
+    watchEffect(() => {
+      console.log('watchEffect function ran', search.value)
+    })
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return {name, age, handleClick, names, search, matchingNames}
   }
 }
 </script>
